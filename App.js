6176a065b0cc32fabc41a84c25dd7d2e84ce6f8b
VoVyKhang <vo.vykhang24@gmail.com>
2023-06-21 10:03:49 +0700

    Setup

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import {StyleSheet, Text, View} from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import HomeScreen from './src/screens/HomeScreen'
+import DetailScreen from './src/screens/DetailScreen'
+import {Icon} from 'react-native-elements'
+import FavoriteScreen from './src/screens/FavoriteScreen'
+
+const Tab = createBottomTabNavigator()
+const Stack = createNativeStackNavigator()
+
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="Detail" component={DetailScreen} />
+  </Stack.Navigator>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => <Icon name="home" type="font-awesome" color={color} />,
+          }}
+        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Tab.Screen
+          name="Favorites"
+          component={FavoriteScreen}
+          options={{
+            // headerShown: false,
+            tabBarLabel: 'Favorites',
+            tabBarIcon: ({color}) => <Icon name="heart" type="font-awesome" color={color} />,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
